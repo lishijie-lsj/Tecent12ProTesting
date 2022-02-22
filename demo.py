d0399f8c190fdf10a715768e6b1567dbb8c2e682
shijili <672813112@qq.com>
2022-02-22 18:57:19 +0800

    first commit

---
+++ demo.py
@@ -0,0 +1,11 @@
+from python.demo1 import Demo1
+# from testing import demo2, demo3
+from testing.demo2 import Demo2
+from testing import *
+Demo1()
+#Demo2()
+print(demo2.hello)
+
+demo2.f()
+
+demo3
